a9d6ecac99f5cd74d8ffb4bacd74b020c74217ca
William Hardee <wiha7618@colorado.edu>
2024-04-04 16:10:49 -1200

    initial test + fixed file structure

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -173,7 +173,9 @@ app.get('/logout', (req, res) => {
   res.render('pages/logout');
 })
 
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
 
-
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
